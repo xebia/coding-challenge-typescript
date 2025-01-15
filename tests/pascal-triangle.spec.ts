50644e482e09a1559c6e5599a9fb646fd8da85cf
Ruben Oostinga <ruben.oostinga@ing.com>
2025-01-15 16:28:54 +0100

    Migrate to vitest for speed

--- tests/pascal-triangle.spec.ts
+++ tests/pascal-triangle.spec.ts
@@ -1,4 +1,5 @@
 import {triangleRow} from '../src/pascal-triangle';
+import {describe, expect, it} from "vitest";
 
 describe("Pascal's Triangle", () => {
     it(`should return pascal row 0`, () => {
@@ -6,18 +7,18 @@ describe("Pascal's Triangle", () => {
     });
 
     it(`should return pascal row 1`, () => {
-        expect(triangleRow(1)).toEqual([1,1])
+        expect(triangleRow(1)).toEqual([1, 1])
     });
 
     it(`should return pascal row 2`, () => {
-        expect(triangleRow(2)).toEqual([1,2,1])
+        expect(triangleRow(2)).toEqual([1, 2, 1])
     });
 
     it(`should return pascal row 3`, () => {
-        expect(triangleRow(3)).toEqual([1,3,3,1])
+        expect(triangleRow(3)).toEqual([1, 3, 3, 1])
     });
 
     it(`should return pascal row 4`, () => {
-        expect(triangleRow(4)).toEqual([1,4,6,4,1])
+        expect(triangleRow(4)).toEqual([1, 4, 6, 4, 1])
     });
 });
